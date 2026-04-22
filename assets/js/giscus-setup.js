2b3896f62e4fe7816ab46077bf167112ba615a83
l-Galland <l-Galland@users.noreply.github.com>
2026-04-22 09:07:16 +0000

    Deploying to gh-pages from @ l-Galland/Personal-Organization-site@bc6b1b3d5eb98b0b3db7a15bebf2cf947bf69409 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "l-Galland/PersonalWebSite",
+    "data-repo": "l-Galland/Personal-Organization-site",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
